f415518ff8b88280f0ec45a2465aebe16cf60fbd
kamblebhavesh0123-hub <kamblebhavesh0123@gmail.com>
2026-07-28 17:56:32 +0530

    Update SkyMinionsPlugin.java

--- src/main/java/com/skyminions/SkyMinionsPlugin.java
+++ src/main/java/com/skyminions/SkyMinionsPlugin.java
@@ -1,74 +1,43 @@
 package com.skyminions;
 
 import com.skyminions.commands.MinionCommand;
+import com.skyminions.config.MinionConfigManager;
 import com.skyminions.events.MinionListener;
 import com.skyminions.gui.GUIListener;
 import com.skyminions.gui.GUIManager;
-import com.skyminions.hooks.PluginHooks;
-import com.skyminions.managers.MinionManager;
+import com.skyminions.minions.MinionManager;
 import com.skyminions.tasks.MinionTask;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class SkyMinionsPlugin extends JavaPlugin {
+public class SkyMinionsPlugin extends JavaPlugin {
 
-    private static SkyMinionsPlugin instance;
     private MinionManager minionManager;
     private GUIManager guiManager;
-    private PluginHooks pluginHooks;
-    private MinionTask minionTask;
+    private MinionConfigManager configManager;
 
     @Override
     public void onEnable() {
-        instance = this;
         saveDefaultConfig();
 
-        this.minionManager = new MinionManager(this);
+        this.configManager = new MinionConfigManager(this);
+        this.minionManager = new MinionManager();
         this.guiManager = new GUIManager(this);
-        this.pluginHooks = new PluginHooks(this);
 
-        // Start background work task (runs every 3 seconds)
-        this.minionTask = new MinionTask(this);
-        this.minionTask.runTaskTimer(this, 60L, 60L);
-
-        // Register Events & Listeners
-        getServer().getPluginManager().registerEvents(new MinionListener(this), this);
-        getServer().getPluginManager().registerEvents(new GUIListener(this), this);
-
-        // Register Commands
-        MinionCommand minionCommand = new MinionCommand(this);
         if (getCommand("minion") != null) {
+            MinionCommand minionCommand = new MinionCommand(this);
             getCommand("minion").setExecutor(minionCommand);
             getCommand("minion").setTabCompleter(minionCommand);
         }
 
-        getLogger().info("SkyMinions 1.21 full release build ready!");
+        getServer().getPluginManager().registerEvents(new MinionListener(this), this);
+        getServer().getPluginManager().registerEvents(new GUIListener(this), this);
+
+        new MinionTask(this).runTaskTimer(this, 60L, 60L);
+
+        getLogger().info("SkyMinions Plugin Enabled Successfully!");
     }
 
-    @Override
-    public void onDisable() {
-        if (minionTask != null) {
-            minionTask.cancel();
-        }
-        if (minionManager != null) {
-            minionManager.saveMinions();
-        }
-        getLogger().info("SkyMinions data saved and disabled gracefully.");
-    }
-
-    public static SkyMinionsPlugin getInstance() {
-        return instance;
-    }
-
-    public MinionManager getMinionManager() {
-        return minionManager;
-    }
-
-    public GUIManager getGuiManager() {
-        return guiManager;
-    }
-
-    public PluginHooks getPluginHooks() {
-        return pluginHooks;
-    }
-            }
-        
+    public MinionManager getMinionManager() { return minionManager; }
+    public GUIManager getGuiManager() { return guiManager; }
+    public MinionConfigManager getConfigManager() { return configManager; }
+}
