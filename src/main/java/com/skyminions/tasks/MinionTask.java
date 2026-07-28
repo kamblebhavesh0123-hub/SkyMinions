8b17c57af6e096b0ce4d31109b3d12cda937fe47
kamblebhavesh0123-hub <kamblebhavesh0123@gmail.com>
2026-07-28 19:24:49 +0530

    Update MinionTask.java

--- src/main/java/com/skyminions/tasks/MinionTask.java
+++ src/main/java/com/skyminions/tasks/MinionTask.java
@@ -2,8 +2,12 @@ package com.skyminions.tasks;
 
 import com.skyminions.SkyMinionsPlugin;
 import com.skyminions.models.Minion;
+import com.skyminions.models.MinionConfig;
+import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
+import org.bukkit.block.Block;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class MinionTask extends BukkitRunnable {
@@ -17,26 +21,34 @@ public class MinionTask extends BukkitRunnable {
     @Override
     public void run() {
         for (Minion minion : plugin.getMinionManager().getAllMinions()) {
-            if (minion.getLocation() != null && minion.getLocation().getWorld() != null) {
-                int maxCapacity = minion.getLevel() * 128;
-                if (minion.getStoredAmount() < maxCapacity) {
-                    // Produce 2x resources if fuel is active, otherwise 1
+            Location loc = minion.getLocation();
+            if (loc == null || loc.getWorld() == null) continue;
+
+            MinionConfig config = plugin.getConfigManager().getMinionConfig(minion.getType());
+            Material targetBlockMat = config != null ? config.getResourceMaterial() : Material.COBBLESTONE;
+
+            // Target block directly in front of minion
+            Block targetBlock = loc.clone().add(loc.getDirection().multiply(1.5)).getBlock();
+
+            int maxCapacity = minion.getLevel() * 128;
+            if (minion.getStoredAmount() < maxCapacity) {
+
+                // Step A: If target space is empty or grass, place target resource block
+                if (targetBlock.getType() == Material.AIR || targetBlock.getType() == Material.GRASS_BLOCK) {
+                    targetBlock.setType(targetBlockMat);
+                    loc.getWorld().playSound(targetBlock.getLocation(), Sound.BLOCK_STONE_PLACE, 0.5f, 1.0f);
+                } 
+                // Step B: Break target resource block & collect resource
+                else if (targetBlock.getType() == targetBlockMat) {
+                    loc.getWorld().spawnParticle(Particle.BLOCK, targetBlock.getLocation().add(0.5, 0.5, 0.5), 15, targetBlockMat.createBlockData());
+                    loc.getWorld().playSound(targetBlock.getLocation(), Sound.BLOCK_STONE_BREAK, 0.8f, 1.2f);
+                    targetBlock.setType(Material.AIR);
+
                     int amountToProduce = minion.hasFuel() ? 2 : 1;
                     minion.addStoredAmount(amountToProduce);
-
-                    minion.getLocation().getWorld().spawnParticle(
-                            Particle.HAPPY_VILLAGER,
-                            minion.getLocation().clone().add(0, 1.2, 0),
-                            minion.hasFuel() ? 8 : 3, 0.2, 0.2, 0.2
-                    );
-                    minion.getLocation().getWorld().playSound(
-                            minion.getLocation(),
-                            Sound.BLOCK_STONE_BREAK,
-                            0.5f, 1.2f
-                    );
                 }
             }
         }
     }
-                    }
-                        
+    }
+                
