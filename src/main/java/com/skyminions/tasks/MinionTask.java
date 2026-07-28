7e2db3621244aa5c189a915fdaf2ab180278d0cd
kamblebhavesh0123-hub <kamblebhavesh0123@gmail.com>
2026-07-28 23:06:03 +0530

    Update MinionTask.java

--- src/main/java/com/skyminions/tasks/MinionTask.java
+++ src/main/java/com/skyminions/tasks/MinionTask.java
@@ -1,6 +1,7 @@
 package com.skyminions.tasks;
 
 import com.skyminions.SkyMinionsPlugin;
+import com.skyminions.minions.MinionEntity;
 import com.skyminions.models.Minion;
 import com.skyminions.models.MinionConfig;
 import org.bukkit.Location;
@@ -8,6 +9,8 @@ import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.block.Block;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Entity;
 import org.bukkit.scheduler.BukkitRunnable;
 
 public class MinionTask extends BukkitRunnable {
@@ -27,25 +30,49 @@ public class MinionTask extends BukkitRunnable {
             MinionConfig config = plugin.getConfigManager().getMinionConfig(minion.getType());
             Material targetBlockMat = config != null ? config.getResourceMaterial() : Material.COBBLESTONE;
 
-            // Compute block offset 1 block ahead in facing direction
+            // Target block 1.2 units ahead of minion
             Block targetBlock = loc.clone().add(loc.getDirection().multiply(1.2)).getBlock();
 
-            int maxCapacity = minion.getLevel() * 128;
-            if (minion.getStoredAmount() < maxCapacity) {
-
-                if (targetBlock.getType() == Material.AIR || targetBlock.getType() == Material.SHORT_GRASS || targetBlock.getType() == Material.TALL_GRASS) {
-                    targetBlock.setType(targetBlockMat);
-                    loc.getWorld().playSound(targetBlock.getLocation(), Sound.BLOCK_STONE_PLACE, 0.5f, 1.0f);
-                } else if (targetBlock.getType() == targetBlockMat) {
-                    loc.getWorld().spawnParticle(Particle.BLOCK, targetBlock.getLocation().add(0.5, 0.5, 0.5), 10, targetBlockMat.createBlockData());
-                    loc.getWorld().playSound(targetBlock.getLocation(), Sound.BLOCK_STONE_BREAK, 0.8f, 1.2f);
-                    targetBlock.setType(Material.AIR);
-
-                    int amountToProduce = minion.hasFuel() ? 2 : 1;
-                    minion.addStoredAmount(amountToProduce);
+            // Find matching ArmorStand nearby for visual animations
+            ArmorStand stand = null;
+            for (Entity entity : loc.getWorld().getNearbyEntities(loc, 1.5, 1.5, 1.5)) {
+                if (entity instanceof ArmorStand armorStand) {
+                    stand = armorStand;
+                    break;
                 }
+            }
+
+            if (minion.getStorage().isFull()) {
+                // Storage Full -> Spawn Smoke particles & update tag
+                MinionEntity.playParticleFeedback(loc, false);
+                if (stand != null) {
+                    MinionEntity.updateNameTag(minion, stand);
+                }
+                continue;
+            }
+
+            // Step A: Place block if empty
+            if (targetBlock.getType() == Material.AIR || targetBlock.getType() == Material.SHORT_GRASS || targetBlock.getType() == Material.TALL_GRASS) {
+                targetBlock.setType(targetBlockMat);
+                loc.getWorld().playSound(targetBlock.getLocation(), Sound.BLOCK_STONE_PLACE, 0.5f, 1.0f);
+            } 
+            // Step B: Mine block, trigger arm swing & collect item
+            else if (targetBlock.getType() == targetBlockMat) {
+                loc.getWorld().spawnParticle(Particle.BLOCK, targetBlock.getLocation().add(0.5, 0.5, 0.5), 10, targetBlockMat.createBlockData());
+                loc.getWorld().playSound(targetBlock.getLocation(), Sound.BLOCK_STONE_BREAK, 0.8f, 1.2f);
+                targetBlock.setType(Material.AIR);
+
+                int amountToProduce = minion.hasFuel() ? 2 : 1;
+                minion.addStoredAmount(amountToProduce);
+
+                // Trigger Visual Feedback & Live Name Tag Update
+                if (stand != null) {
+                    MinionEntity.playCollectAnimation(plugin, stand);
+                    MinionEntity.updateNameTag(minion, stand);
+                }
+                MinionEntity.playParticleFeedback(loc, true);
             }
         }
     }
-                }
-                                             
+                    }
+                    
