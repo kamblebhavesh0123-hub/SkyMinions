c65c283e46b71e37c185a13b51e7a3ba02d2b896
kamblebhavesh0123-hub <kamblebhavesh0123@gmail.com>
2026-07-28 22:40:12 +0530

    Update MinionConfig.java

--- src/main/java/com/skyminions/models/MinionConfig.java
+++ src/main/java/com/skyminions/models/MinionConfig.java
@@ -4,22 +4,24 @@ import org.bukkit.Color;
 import org.bukkit.Material;
 
 public class MinionConfig {
+    private String type;
+    private String displayName;
+    private Material resourceMaterial;
+    private Material toolMaterial;
+    private Material heldItemFallback;
+    private String headTexture;
+    private Color chestplateColor;
+    private int shopSlot;
 
-    private final String type;
-    private final String displayName;
-    private final Material resourceMaterial;
-    private final Material toolMaterial;
-    private final String headTexture;
-    private final Color armorColor;
-    private final int shopSlot;
-
-    public MinionConfig(String type, String displayName, Material resourceMaterial, Material toolMaterial, String headTexture, Color armorColor, int shopSlot) {
+    public MinionConfig(String type, String displayName, Material resourceMaterial, Material toolMaterial, 
+                        Material heldItemFallback, String headTexture, Color chestplateColor, int shopSlot) {
         this.type = type;
         this.displayName = displayName;
         this.resourceMaterial = resourceMaterial;
         this.toolMaterial = toolMaterial;
+        this.heldItemFallback = heldItemFallback;
         this.headTexture = headTexture;
-        this.armorColor = armorColor;
+        this.chestplateColor = chestplateColor;
         this.shopSlot = shopSlot;
     }
 
@@ -27,7 +29,8 @@ public class MinionConfig {
     public String getDisplayName() { return displayName; }
     public Material getResourceMaterial() { return resourceMaterial; }
     public Material getToolMaterial() { return toolMaterial; }
+    public Material getHeldItemFallback() { return heldItemFallback; }
     public String getHeadTexture() { return headTexture; }
-    public Color getArmorColor() { return armorColor; }
+    public Color getChestplateColor() { return chestplateColor; }
     public int getShopSlot() { return shopSlot; }
-  }
+}
