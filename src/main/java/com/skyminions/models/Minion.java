e2fb372171618981f34d7dedc60d2e270c192045
kamblebhavesh0123-hub <kamblebhavesh0123@gmail.com>
2026-07-28 23:52:03 +0530

    Update Minion.java

--- src/main/java/com/skyminions/models/Minion.java
+++ src/main/java/com/skyminions/models/Minion.java
@@ -7,6 +7,7 @@ public class Minion extends AbstractMinion {
 
     private boolean hasSmelter;
     private boolean hasCompactor;
+    private boolean hasFuel;
 
     public Minion(UUID minionId, UUID ownerId, String type, int level, Location location) {
         super(minionId, ownerId, type, level, location);
@@ -17,9 +18,30 @@ public class Minion extends AbstractMinion {
         // Individual tick logic managed by centralized MinionTickerTask
     }
 
+    // Direct helper methods for stored amounts used by GUIs & Tasks
+    public int getStoredAmount() { 
+        return storage != null ? storage.getStoredAmount() : 0; 
+    }
+
+    public void setStoredAmount(int amount) { 
+        if (storage != null) {
+            storage.setStoredAmount(amount); 
+        }
+    }
+
+    public void addStoredAmount(int amount) { 
+        if (storage != null) {
+            storage.setStoredAmount(getStoredAmount() + amount); 
+        }
+    }
+
+    // Upgrade Getters & Setters
     public boolean hasSmelter() { return hasSmelter; }
     public void setHasSmelter(boolean hasSmelter) { this.hasSmelter = hasSmelter; }
 
     public boolean hasCompactor() { return hasCompactor; }
     public void setHasCompactor(boolean hasCompactor) { this.hasCompactor = hasCompactor; }
-}
+
+    public boolean hasFuel() { return hasFuel || fuelRemainingMillis > 0; }
+    public void setHasFuel(boolean hasFuel) { this.hasFuel = hasFuel; }
+        }
