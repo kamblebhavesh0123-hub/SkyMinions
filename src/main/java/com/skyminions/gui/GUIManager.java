4b0153fba59ebe7333dbc9eedf6603a441392a8a
kamblebhavesh0123-hub <kamblebhavesh0123@gmail.com>
2026-07-28 17:00:22 +0530

    Update GUIManager.java

--- src/main/java/com/skyminions/gui/GUIManager.java
+++ src/main/java/com/skyminions/gui/GUIManager.java
@@ -2,6 +2,7 @@ package com.skyminions.gui;
 
 import com.skyminions.SkyMinionsPlugin;
 import com.skyminions.models.Minion;
+import com.skyminions.util.ItemUtil;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
@@ -22,37 +23,60 @@ public class GUIManager {
         this.plugin = plugin;
     }
 
-    public void openMainMenu(Player player, Minion minion) {
-        Component title = Component.text(minion.getType() + " Minion [Lv." + minion.getLevel() + "]", NamedTextColor.DARK_GRAY);
-        Inventory gui = Bukkit.createInventory(null, 54, title);
+    public void openShopMenu(Player player) {
+        Component title = Component.text("Minion Shop", NamedTextColor.DARK_GRAY, TextDecoration.BOLD);
+        Inventory gui = Bukkit.createInventory(null, 27, title);
 
-        // Fill background with glass panes
-        ItemStack filler = createGuiItem(Material.GRAY_STAINED_GLASS_PANE, Component.text(" ", NamedTextColor.GRAY));
-        for (int i = 0; i < 54; i++) {
-            gui.setItem(i, filler);
+        ItemStack border = createGuiItem(Material.GRAY_STAINED_GLASS_PANE, Component.text(" ", NamedTextColor.GRAY));
+        for (int i = 0; i < 27; i++) {
+            gui.setItem(i, border);
         }
 
-        // Action Slots
-        gui.setItem(21, createGuiItem(Material.CHEST, 
-                Component.text("Collect Items", NamedTextColor.GREEN, TextDecoration.BOLD),
-                Component.text("Click to collect stored resources.", NamedTextColor.GRAY)));
+        gui.setItem(11, ItemUtil.createMinionItem("COBBLESTONE", 1));
+        gui.setItem(13, ItemUtil.createMinionItem("WHEAT", 1));
+        gui.setItem(15, ItemUtil.createMinionItem("OAK", 1));
 
-        gui.setItem(23, createGuiItem(Material.ANVIL, 
-                Component.text("Upgrade Minion", NamedTextColor.GOLD, TextDecoration.BOLD),
-                Component.text("Current Level: ", NamedTextColor.GRAY).append(Component.text(minion.getLevel(), NamedTextColor.YELLOW)),
-                Component.text("Click to upgrade speed & storage.", NamedTextColor.GRAY)));
+        player.openInventory(gui);
+    }
 
-        gui.setItem(30, createGuiItem(Material.BLAZE_POWDER, 
-                Component.text("Fuel Slot", NamedTextColor.RED, TextDecoration.BOLD),
-                Component.text("Add fuel to increase minion speed.", NamedTextColor.GRAY)));
+    public void openMainMenu(Player player, Minion minion) {
+        String romanLevel = toRoman(minion.getLevel());
+        Component title = Component.text(minion.getType() + " Minion " + romanLevel, NamedTextColor.DARK_GRAY);
+        Inventory gui = Bukkit.createInventory(null, 54, title);
 
-        gui.setItem(32, createGuiItem(Material.DIAMOND_PICKAXE, 
-                Component.text("Equipment Slot", NamedTextColor.AQUA, TextDecoration.BOLD),
-                Component.text("Attach compactors or auto-smelters.", NamedTextColor.GRAY)));
+        ItemStack border = createGuiItem(Material.GRAY_STAINED_GLASS_PANE, Component.text(" ", NamedTextColor.GRAY));
+        for (int i = 0; i < 54; i++) {
+            gui.setItem(i, border);
+        }
 
-        gui.setItem(49, createGuiItem(Material.REDSTONE_BLOCK, 
-                Component.text("Pick Up Minion", NamedTextColor.RED, TextDecoration.BOLD),
-                Component.text("Reclaim this minion into your inventory.", NamedTextColor.GRAY)));
+        gui.setItem(4, createGuiItem(Material.REDSTONE_TORCH, 
+                Component.text("Minion Info", NamedTextColor.RED, TextDecoration.BOLD),
+                Component.text("Speed: ", NamedTextColor.GRAY).append(Component.text("14s", NamedTextColor.GREEN))));
+
+        gui.setItem(5, createGuiItem(Material.PLAYER_HEAD, 
+                Component.text(minion.getType() + " Minion", NamedTextColor.AQUA, TextDecoration.BOLD)));
+
+        gui.setItem(10, createGuiItem(Material.LIME_STAINED_GLASS_PANE, 
+                Component.text("Auto Sell Slot", NamedTextColor.GREEN, TextDecoration.BOLD)));
+
+        gui.setItem(19, createGuiItem(Material.ORANGE_STAINED_GLASS_PANE, 
+                Component.text("Fuel Slot", NamedTextColor.GOLD, TextDecoration.BOLD)));
+
+        gui.setItem(28, createGuiItem(Material.BLUE_STAINED_GLASS_PANE, 
+                Component.text("Compactor Slot", NamedTextColor.BLUE, TextDecoration.BOLD)));
+
+        gui.setItem(37, createGuiItem(Material.YELLOW_STAINED_GLASS_PANE, 
+                Component.text("Upgrade Slot", NamedTextColor.YELLOW, TextDecoration.BOLD)));
+
+        ItemStack storageSlot = createGuiItem(Material.LIGHT_GRAY_STAINED_GLASS_PANE, Component.text("Empty Storage Slot", NamedTextColor.GRAY));
+        int[] storageSlots = {21, 22, 23, 24, 25, 30, 31, 32, 33, 34, 39, 40, 41, 42, 43};
+        for (int slot : storageSlots) {
+            gui.setItem(slot, storageSlot);
+        }
+
+        gui.setItem(48, createGuiItem(Material.CHEST, Component.text("Collect All", NamedTextColor.GREEN, TextDecoration.BOLD)));
+        gui.setItem(50, createGuiItem(Material.DIAMOND, Component.text("Next Tier Upgrade", NamedTextColor.AQUA, TextDecoration.BOLD)));
+        gui.setItem(52, createGuiItem(Material.BEDROCK, Component.text("Pickup Minion", NamedTextColor.RED, TextDecoration.BOLD)));
 
         player.openInventory(gui);
     }
@@ -62,11 +86,17 @@ public class GUIManager {
         ItemMeta meta = item.getItemMeta();
         if (meta != null) {
             meta.displayName(name);
-            if (lore.length > 0) {
-                meta.lore(List.of(lore));
-            }
+            if (lore.length > 0) meta.lore(List.of(lore));
             item.setItemMeta(meta);
         }
         return item;
     }
-}
+
+    private static String toRoman(int number) {
+        return switch (number) {
+            case 1 -> "I"; case 2 -> "II"; case 3 -> "III"; case 4 -> "IV"; case 5 -> "V";
+            default -> String.valueOf(number);
+        };
+    }
+                                     }
+            
