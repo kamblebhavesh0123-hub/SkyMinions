96d2f90391c253c721663d3b06d071fdf3422258
kamblebhavesh0123-hub <kamblebhavesh0123@gmail.com>
2026-07-28 16:00:12 +0530

    Update SkyMinionsPlugin.java

--- src/main/java/com/skyminions/SkyMinionsPlugin.java
+++ src/main/java/com/skyminions/SkyMinionsPlugin.java
@@ -2,6 +2,8 @@ package com.skyminions;
 
 import com.skyminions.commands.MinionCommand;
 import com.skyminions.events.MinionListener;
+import com.skyminions.gui.GUIListener;
+import com.skyminions.gui.GUIManager;
 import com.skyminions.managers.MinionManager;
 import com.skyminions.tasks.MinionTask;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -10,6 +12,7 @@ public final class SkyMinionsPlugin extends JavaPlugin {
 
     private static SkyMinionsPlugin instance;
     private MinionManager minionManager;
+    private GUIManager guiManager;
     private MinionTask minionTask;
 
     @Override
@@ -18,22 +21,24 @@ public final class SkyMinionsPlugin extends JavaPlugin {
         saveDefaultConfig();
 
         this.minionManager = new MinionManager(this);
+        this.guiManager = new GUIManager(this);
 
         // Start background work task (runs every 3 seconds)
         this.minionTask = new MinionTask(this);
         this.minionTask.runTaskTimer(this, 60L, 60L);
 
-        // Register Events
+        // Register Events & Listeners
         getServer().getPluginManager().registerEvents(new MinionListener(this), this);
+        getServer().getPluginManager().registerEvents(new GUIListener(this), this);
 
-        // Register Command Executor and Tab Completer
+        // Register Commands
         MinionCommand minionCommand = new MinionCommand(this);
         if (getCommand("minion") != null) {
             getCommand("minion").setExecutor(minionCommand);
             getCommand("minion").setTabCompleter(minionCommand);
         }
 
-        getLogger().info("SkyMinions 1.21 full core, events, and commands loaded!");
+        getLogger().info("SkyMinions 1.21 full core, GUIs, events, and commands loaded!");
     }
 
     @Override
@@ -54,5 +59,8 @@ public final class SkyMinionsPlugin extends JavaPlugin {
     public MinionManager getMinionManager() {
         return minionManager;
     }
-            }
-    
+
+    public GUIManager getGuiManager() {
+        return guiManager;
+    }
+}
