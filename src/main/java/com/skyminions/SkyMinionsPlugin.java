e522982a095cb13eda2fbf076c32426125448c9d
kamblebhavesh0123-hub <kamblebhavesh0123@gmail.com>
2026-07-28 15:40:46 +0530

    Update SkyMinionsPlugin.java

--- src/main/java/com/skyminions/SkyMinionsPlugin.java
+++ src/main/java/com/skyminions/SkyMinionsPlugin.java
@@ -1,12 +1,14 @@
 package com.skyminions;
 
 import com.skyminions.managers.MinionManager;
+import com.skyminions.tasks.MinionTask;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SkyMinionsPlugin extends JavaPlugin {
 
     private static SkyMinionsPlugin instance;
     private MinionManager minionManager;
+    private MinionTask minionTask;
 
     @Override
     public void onEnable() {
@@ -15,11 +17,18 @@ public final class SkyMinionsPlugin extends JavaPlugin {
 
         this.minionManager = new MinionManager(this);
 
-        getLogger().info("SkyMinions 1.21 core managers initialized successfully!");
+        // Start background work task (runs every 3 seconds)
+        this.minionTask = new MinionTask(this);
+        this.minionTask.runTaskTimer(this, 60L, 60L);
+
+        getLogger().info("SkyMinions 1.21 core managers and tasks initialized!");
     }
 
     @Override
     public void onDisable() {
+        if (minionTask != null) {
+            minionTask.cancel();
+        }
         if (minionManager != null) {
             minionManager.saveMinions();
         }
