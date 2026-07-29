72a96022358fccdfe8bbf9ef9fd8aa6b4119c33a
kamblebhavesh0123-hub <kamblebhavesh0123@gmail.com>
2026-07-29 16:55:21 +0530

    Create Fuel.java

---
+++ src/main/java/com/skyminions/models/Fuel.java
@@ -0,0 +1,36 @@
+package com.skyminions.models;
+
+public class Fuel {
+
+    private final String id;
+    private final String displayName;
+    private final double speedMultiplier;
+    private final long durationSeconds;
+
+    public Fuel(String id, String displayName, double speedMultiplier, long durationSeconds) {
+        this.id = id;
+        this.displayName = displayName;
+        this.speedMultiplier = speedMultiplier;
+        this.durationSeconds = durationSeconds;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public double getSpeedMultiplier() {
+        return speedMultiplier;
+    }
+
+    public long getDurationSeconds() {
+        return durationSeconds;
+    }
+
+    public boolean isPermanent() {
+        return durationSeconds < 0;
+    }
+}
