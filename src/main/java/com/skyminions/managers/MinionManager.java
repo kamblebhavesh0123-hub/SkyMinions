89ce25dd12ed9e224d3fe97963c48092b10be1f3
kamblebhavesh0123-hub <kamblebhavesh0123@gmail.com>
2026-07-29 12:32:29 +0530

    Update MinionManager.java

--- src/main/java/com/skyminions/managers/MinionManager.java
+++ src/main/java/com/skyminions/managers/MinionManager.java
@@ -2,97 +2,57 @@ package com.skyminions.managers;
 
 import com.skyminions.SkyMinionsPlugin;
 import com.skyminions.models.Minion;
-import org.bukkit.Location;
-import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.configuration.file.YamlConfiguration;
+import com.skyminions.storage.StorageRepository;
+import com.skyminions.storage.YAMLStorageRepository;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.*;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
 public class MinionManager {
 
     private final SkyMinionsPlugin plugin;
     private final Map<UUID, Minion> activeMinions = new HashMap<>();
-    private File dataFile;
-    private FileConfiguration dataConfig;
+    private final StorageRepository storageRepository;
 
     public MinionManager(SkyMinionsPlugin plugin) {
         this.plugin = plugin;
-        setupDataFile();
+        this.storageRepository = new YAMLStorageRepository(plugin);
+        this.storageRepository.init();
         loadMinions();
     }
 
-    private void setupDataFile() {
-        dataFile = new File(plugin.getDataFolder(), "data/minions_data.yml");
-        if (!dataFile.exists()) {
-            dataFile.getParentFile().mkdirs();
-            try {
-                dataFile.createNewFile();
-            } catch (IOException e) {
-                plugin.getLogger().severe("Could not create minions_data.yml!");
-            }
-        }
-        dataConfig = YamlConfiguration.loadConfiguration(dataFile);
-    }
-
-    public void addMinion(Minion minion) {
+    public void registerMinion(Minion minion) {
         activeMinions.put(minion.getMinionId(), minion);
-        saveMinions();
+        storageRepository.saveMinion(minion);
     }
 
-    public void removeMinion(UUID id) {
-        activeMinions.remove(id);
-        dataConfig.set("minions." + id.toString(), null);
-        saveDataFile();
+    public void removeMinion(UUID minionId) {
+        activeMinions.remove(minionId);
+        storageRepository.deleteMinion(minionId);
     }
 
-    public Minion getMinion(UUID id) {
-        return activeMinions.get(id);
+    public Minion getMinion(UUID minionId) {
+        return activeMinions.get(minionId);
     }
 
     public Collection<Minion> getAllMinions() {
         return activeMinions.values();
     }
 
+    public void loadMinions() {
+        activeMinions.clear();
+        for (Minion minion : storageRepository.loadAllMinions()) {
+            activeMinions.put(minion.getMinionId(), minion);
+        }
+    }
+
     public void saveMinions() {
-        for (Minion minion : activeMinions.values()) {
-            String path = "minions." + minion.getMinionId().toString() + ".";
-            dataConfig.set(path + "owner", minion.getOwnerUUID().toString());
-            dataConfig.set(path + "type", minion.getType());
-            dataConfig.set(path + "level", minion.getLevel());
-            dataConfig.set(path + "location", minion.getLocation());
-        }
-        saveDataFile();
+        storageRepository.saveAllMinions(activeMinions.values());
     }
 
-    private void loadMinions() {
-        ConfigurationSection section = dataConfig.getConfigurationSection("minions");
-        if (section == null) return;
-
-        for (String key : section.getKeys(false)) {
-            UUID id = UUID.fromString(key);
-            String path = "minions." + key + ".";
-            String ownerStr = dataConfig.getString(path + "owner");
-            if (ownerStr == null) continue;
-
-            UUID owner = UUID.fromString(ownerStr);
-            String type = dataConfig.getString(path + "type");
-            int level = dataConfig.getInt(path + "level");
-            Location loc = dataConfig.getLocation(path + "location");
-
-            Minion minion = new Minion(id, owner, type, level, loc);
-            activeMinions.put(id, minion);
-        }
+    public StorageRepository getStorageRepository() {
+        return storageRepository;
     }
-
-    private void saveDataFile() {
-        try {
-            dataConfig.save(dataFile);
-        } catch (IOException e) {
-            plugin.getLogger().severe("Could not save minions_data.yml!");
-        }
-    }
-    }
-                
+}
