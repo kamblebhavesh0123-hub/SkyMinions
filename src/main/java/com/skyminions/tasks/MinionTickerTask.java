b0326b1f052f53002a22dcb409db0fc584849083
kamblebhavesh0123-hub <kamblebhavesh0123@gmail.com>
2026-07-29 12:13:35 +0530

    Update MinionTickerTask.java

--- src/main/java/com/skyminions/tasks/MinionTickerTask.java
+++ src/main/java/com/skyminions/tasks/MinionTickerTask.java
@@ -5,6 +5,7 @@ import com.skyminions.api.events.MinionGenerateEvent;
 import com.skyminions.minions.MinionEntity;
 import com.skyminions.models.Minion;
 import com.skyminions.models.MinionConfig;
+import com.skyminions.models.state.MinionState;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
@@ -34,10 +35,8 @@ public class MinionTickerTask extends BukkitRunnable {
             Location loc = minion.getLocation();
             if (loc == null || loc.getWorld() == null || !loc.getChunk().isLoaded()) continue;
 
-            MinionConfig config = plugin.getConfigManager().getMinionConfig(minion.getType());
-            Material targetBlockMat = config != null ? config.getResourceMaterial() : Material.COBBLESTONE;
-
-            Block targetBlock = loc.clone().add(loc.getDirection().multiply(1.2)).getBlock();
+            // Evaluate Minion State Machine
+            minion.getStateMachine().evaluateState();
 
             ArmorStand stand = null;
             for (Entity entity : loc.getWorld().getNearbyEntities(loc, 1.5, 1.5, 1.5)) {
@@ -47,7 +46,8 @@ public class MinionTickerTask extends BukkitRunnable {
                 }
             }
 
-            if (minion.getStorage().isFull()) {
+            // If storage is full, play particle feedback and throttle processing
+            if (minion.getState() == MinionState.STORAGE_FULL) {
                 MinionEntity.playParticleFeedback(loc, false);
                 if (stand != null) {
                     MinionEntity.updateNameTag(minion, stand);
@@ -55,10 +55,17 @@ public class MinionTickerTask extends BukkitRunnable {
                 continue;
             }
 
+            MinionConfig config = plugin.getConfigManager().getMinionConfig(minion.getType());
+            Material targetBlockMat = config != null ? config.getResourceMaterial() : Material.COBBLESTONE;
+
+            Block targetBlock = loc.clone().add(loc.getDirection().multiply(1.2)).getBlock();
+
             if (targetBlock.getType() == Material.AIR || targetBlock.getType() == Material.GRASS_BLOCK || targetBlock.getType() == Material.TALL_GRASS) {
+                minion.getStateMachine().setState(MinionState.WORKING);
                 targetBlock.setType(targetBlockMat);
                 loc.getWorld().playSound(targetBlock.getLocation(), Sound.BLOCK_STONE_PLACE, 0.5f, 1.0f);
             } else if (targetBlock.getType() == targetBlockMat) {
+                minion.getStateMachine().setState(MinionState.COLLECTING);
                 loc.getWorld().spawnParticle(Particle.BLOCK, targetBlock.getLocation().add(0.5, 0.5, 0.5), 10, targetBlockMat.createBlockData());
                 loc.getWorld().playSound(targetBlock.getLocation(), Sound.BLOCK_STONE_BREAK, 0.8f, 1.2f);
                 targetBlock.setType(Material.AIR);
@@ -81,8 +88,10 @@ public class MinionTickerTask extends BukkitRunnable {
                     }
                     MinionEntity.playParticleFeedback(loc, true);
                 }
+            } else {
+                minion.getStateMachine().setState(MinionState.IDLE);
             }
         }
     }
                     }
-        
+                    
