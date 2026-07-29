f98f4e670de18d5dd5a6c0d5702e5a78f11b2b7f
kamblebhavesh0123-hub <kamblebhavesh0123@gmail.com>
2026-07-29 16:48:33 +0530

    Update UpgradeManager.java

--- src/main/java/com/skyminions/managers/UpgradeManager.java
+++ src/main/java/com/skyminions/managers/UpgradeManager.java
@@ -3,16 +3,28 @@ package com.skyminions.managers;
 import com.skyminions.SkyMinionsPlugin;
 import com.skyminions.api.events.MinionUpgradeEvent;
 import com.skyminions.models.Minion;
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
 import org.bukkit.Sound;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.RegisteredServiceProvider;
 
 public class UpgradeManager {
 
     private final SkyMinionsPlugin plugin;
+    private Economy vaultEconomy;
 
     public UpgradeManager(SkyMinionsPlugin plugin) {
         this.plugin = plugin;
+        setupVault();
+    }
+
+    private void setupVault() {
+        if (Bukkit.getPluginManager().getPlugin("Vault") == null) return;
+        RegisteredServiceProvider<Economy> rsp = Bukkit.getServicesManager().getRegistration(Economy.class);
+        if (rsp != null) {
+            this.vaultEconomy = rsp.getProvider();
+        }
     }
 
     public boolean tryUpgradeMinion(Player player, Minion minion) {
@@ -21,15 +33,15 @@ public class UpgradeManager {
 
         double cost = nextLevel * 500.0;
 
-        // Vault Economy Integration check using getHooks()
-        if (plugin.getHooks() != null && plugin.getHooks().hasVault()) {
-            double balance = plugin.getHooks().getVaultEconomy().getBalance(player);
+        // Vault check
+        if (vaultEconomy != null) {
+            double balance = vaultEconomy.getBalance(player);
             if (balance < cost) {
                 player.sendMessage("§cYou don't have enough money! Required: §6$" + cost);
                 player.playSound(player.getLocation(), Sound.ENTITY_VILLAGER_NO, 1.0f, 1.0f);
                 return false;
             }
-            plugin.getHooks().getVaultEconomy().withdrawPlayer(player, cost);
+            vaultEconomy.withdrawPlayer(player, cost);
         }
 
         // Call event
@@ -48,4 +60,4 @@ public class UpgradeManager {
 
         return true;
     }
-    }
+}
