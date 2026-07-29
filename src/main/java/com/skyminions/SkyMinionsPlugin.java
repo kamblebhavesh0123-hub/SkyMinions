7d524374ac58822072cb7406b9ea841b7a7a7b5b
kamblebhavesh0123-hub <kamblebhavesh0123@gmail.com>
2026-07-29 10:45:53 +0530

    Update SkyMinionsPlugin.java

--- src/main/java/com/skyminions/SkyMinionsPlugin.java
+++ src/main/java/com/skyminions/SkyMinionsPlugin.java
@@ -2,6 +2,7 @@ package com.skyminions;
 
 import com.skyminions.commands.MinionCommand;
 import com.skyminions.config.MinionConfigManager;
+import com.skyminions.gui.GUIManager;
 import com.skyminions.managers.MinionManager;
 import com.skyminions.tasks.MinionTickerTask;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -11,6 +12,7 @@ public class SkyMinionsPlugin extends JavaPlugin {
     private static SkyMinionsPlugin instance;
     private MinionConfigManager configManager;
     private MinionManager minionManager;
+    private GUIManager guiManager;
 
     @Override
     public void onEnable() {
@@ -21,9 +23,10 @@ public class SkyMinionsPlugin extends JavaPlugin {
         this.configManager.loadConfigs();
 
         this.minionManager = new MinionManager(this);
+        this.guiManager = new GUIManager(this);
 
         // Register Command Executor & Tab Completer
-        MinionCommand minionCommand = new MinionCommand();
+        MinionCommand minionCommand = new MinionCommand(this);
         if (getCommand("minion") != null) {
             getCommand("minion").setExecutor(minionCommand);
             getCommand("minion").setTabCompleter(minionCommand);
@@ -38,7 +41,7 @@ public class SkyMinionsPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
         if (minionManager != null) {
-            minionManager.saveAllMinions();
+            minionManager.saveMinions();
         }
     }
 
@@ -52,5 +55,9 @@ public class SkyMinionsPlugin extends JavaPlugin {
 
     public MinionManager getMinionManager() { 
         return minionManager; 
+    }
+
+    public GUIManager getGuiManager() {
+        return guiManager;
     }
 }
